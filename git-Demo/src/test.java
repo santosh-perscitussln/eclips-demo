60e7ab11aa448b2fc5f393f3d82b6f07f442223c
Santosh Waghmode <santoshw@perscitussln.com>
2025-05-28 13:33:10 +0530

    html code

--- git-Demo/src/test.java
+++ git-Demo/src/test.java
@@ -4,6 +4,10 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
+StringBuilder sb = new StringBuilder();
+sb.append("Hi, We have received your request. Your account will be unblocked within 48 hours.");
+sb.append("<br><br><a href=\"http://www.google.com\" target=\"_blank\"><button>Click me !</button></a>");
+
 	}
 
 }
